9a7db82e229fda4eb898ce13553d3501bd7b9083
AdvancedGuy <ekidin01@mail.ru>
2025-10-23 15:23:33 +0500

    Use actual pydantic config variant for app config

--- core/config.py
+++ core/config.py
@@ -1,13 +1,12 @@
-from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic import ConfigDict, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     DATABASE_URL: str = Field(default="")
     ECHO: bool = Field(default=True, validation_alias="DATABASE_ECHO")
-
-    class Config:
-        env_file = ".env"
 
 
 config = Config()
